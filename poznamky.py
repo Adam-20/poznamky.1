3ac9b4a618213afd6383689cb3e9a28420eca34e
unknown <adam.lucak@infis.cz>
2025-03-11 08:35:27 +0100

    poznamky

---
+++ poznamky.py
@@ -0,0 +1,32 @@
+#nacteni od uzivatele 
+int(input(""))
+#kdyz potrebujeme neco nacist pouzijeme input
+
+#pretypovani
+cislo=int(input(""))
+#prevedeni z textovy promneny na integer
+
+#podminky
+if cislo<10:
+    print("")
+elif cislo>10:
+    print("")
+else:
+    print("")
+#do podminky napsieme promnenou a pokud se nerovna vypise se elif nebo else 
+
+#logické operátory
+if(cislo<10 and cislo>6):
+    
+#aby cislo melo dve podminky a kdyz je tam not tak se to zneguje 
+
+#vypsat
+    print("ahoj")
+#pro vypsani pouzivame print
+
+#github prikazy
+#git add *
+#git commit -m "text"
+#git push
+
+#Adam Lucák
