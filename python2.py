079146e268dea9663c46430be0a9d196046f4353
unknown <adam.lucak@infis.cz>
2025-03-18 11:24:51 +0100

    hotovo

--- python2.py
+++ python2.py
@@ -8,5 +8,19 @@ vek = int(input())
 print(f"ahoj jmenuji se {jmeno} a je mi {vek}")
 
 #ukol2
-print(f"{prevod:X}")
+print("napis desetine cislo")
+cislo1 = int(input())
+print("napis znamenko")
+zanmenko = input()
+print("napis druhe desetine cislo")
+cislo2 = int(input())
+
+soucet = (cislo1 + cislo2)
+rozdil = (cislo1 - cislo2)
+soucin = (cislo1 * cislo2)
+podil = (cislo1 / cislo2)
+
+vysledek = soucet or rozdil or soucin or podil
+print(vysledek)
+
 
