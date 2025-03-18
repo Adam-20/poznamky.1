bcafacfb26305297b24bbae0fdb65645f240e4ce
unknown <adam.lucak@infis.cz>
2025-03-18 08:35:59 +0100

    ukol 1

---
+++ python2.py
@@ -0,0 +1,12 @@
+#ukol1
+print("napis svoje jmeno")
+jmeno = inpup()
+
+print("napis svuj vek")
+vek = int(input())
+
+print(f"ahoj jmenuji se {jmeno} a je mi {vek}")
+
+#ukol2
+print(f"{prevod:X}")
+
